efbfe5e7d46b0003fcf22bcb6f22df513feb49cc
cakephp-bakeoff <cakephp-bakeoff@outlook.com>
2024-06-03 13:13:25 +0000

    Clarify documentation on cart configuration key

--- config/CakePHPOpencart-dist.php
+++ config/CakePHPOpencart-dist.php
@@ -3,7 +3,7 @@
 return [
     'CakePHPOpencart' => [ // Must match the name of this plugin
         'cartList' => [
-            'ABC' => [ // Arbitrary key used as shorthand to this configuration
+            'ABC' => [ // Arbitrary UPPERCASE key identifying this configuration
                 'name' => 'ABC Shop', // Display name for this cart/multistore
                 'datasource' => 'opencart', // Opencart DB in APP/config/app.php
                 'type' => 'Opencart4', // Opencart2 or Opencart4
